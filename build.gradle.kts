a6fd387f4f80fbf446fed46c02b7be72aa31e1c3
derysudrajat <dery.sudrajat17@gmail.com>
2023-09-25 23:12:39 +0700

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.library").version("8.1.1").apply(false)
-    kotlin("multiplatform").version("1.8.21").apply(false)
+    id("com.android.library").version("7.4.2").apply(false)
+    kotlin("multiplatform").version("1.6.21").apply(false)
 }
 group = "io.github.derysudrajat.kmmlib"
-version = "0.0.1"
+version = "0.0.2"
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
